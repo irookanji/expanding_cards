51a6a023865674d856c2d5b8beba8d1f16db3e34
Bakhtiyar Zubairov <irookanji@gmail.com>
2021-09-17 20:43:50 +0200

    Add pannels plugin method

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
-const panels = document.querySelectorAll(".panel");
+function panelsPlugin(activePanel = 0) {
+  const panels = document.querySelectorAll(".panel");
 
-panels.forEach((panel) => {
-  panel.addEventListener("click", () => {
-    removeActiveClasses();
-    panel.classList.add("active");
-  });
-});
+  panels[activePanel].classList.add("active");
 
-function removeActiveClasses() {
   panels.forEach((panel) => {
-    panel.classList.remove("active");
+    panel.addEventListener("click", () => {
+      removeActiveClasses();
+      panel.classList.add("active");
+    });
   });
+
+  function removeActiveClasses() {
+    panels.forEach((panel) => {
+      panel.classList.remove("active");
+    });
+  }
 }
+
+panelsPlugin(4);
